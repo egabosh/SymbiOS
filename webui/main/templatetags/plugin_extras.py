97239453a979ee3421ac60c4157bd81a1858a916
olli <56364235+egabosh@users.noreply.github.com>
2026-08-24 15:18:48 +0200

    fix: add dict_get template filter for plugin_features.html
    Django templates have no built-in dict key lookup filter.
    Created plugin_extras.py with dict_get filter and updated
    plugin_features.html to use it (replaces invalid |get: filter).

---
+++ webui/main/templatetags/plugin_extras.py
@@ -0,0 +1,27 @@
+# SymbiOS - Debian-based server management platform
+# Copyright (c) 2026, Oliver Bohlen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='dict_get')
+def dict_get(d, key):
+    """Get a value from a dictionary by key. Usage: {{ mydict|dict_get:key }}."""
+    if isinstance(d, dict):
+        return d.get(key, '')
+    return ''
